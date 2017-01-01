c4d5b5ed9dfaeea7f3660e22eed49171632c61ad
gdmec07150907 <570941644@qq.com>
2017-01-01 12:58:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chenpc/simplemediaplayer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.chenpc.simplemediaplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.home_pc.simplemeddiaplayer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
